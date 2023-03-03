94bd4cffd04691e5b8511ecedeeaa3d16c31382a
bijokmathew <bijo.nbr@hotmail.com>
2023-03-03 23:55:04 +0000

    product app: Created product detail template

--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@
 # -----------------------------------------------------------------
 # 3rd Party
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 # internal
@@ -27,3 +27,15 @@ def all_products(request):
         'categories': categories
     }
     return render(request, 'products/products.html', context=context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to show product detail from the
+    the profuct list
+    """
+    product = get_object_or_404(Product, id=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context=context)
