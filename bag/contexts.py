30605de0a020be47f86b74e4a6e562c14362643b
bijokmathew <bijo.nbr@hotmail.com>
2023-03-12 15:11:32 +0000

    Bag-App: Added size option in bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -28,17 +28,30 @@ def bag_contents(request):
     product_count = 0
 
     bag = request.session.get('bag', {})
-    for item_id, quantity in bag.items():
+    for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += product.price * quantity
-        product_count += quantity
-        bag_items.append(
-            {
-                'product': product,
-                'item_id': item_id,
-                'quantity': quantity
-            }
-        )
+        if not isinstance(item_data, int):
+            for size, quantity in item_data['item_by_size'].items():
+                total += product.price * quantity
+                product_count += quantity
+                bag_items.append(
+                    {
+                        'product': product,
+                        'size': size,
+                        'item_id': item_id,
+                        'quantity': quantity
+                    }
+                )
+        else:
+            total += product.price * item_data
+            product_count += item_data
+            bag_items.append(
+                {
+                    'product': product,
+                    'item_id': item_id,
+                    'quantity': item_data
+                }
+            )
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
--- bag/views.py
+++ bag/views.py
@@ -28,10 +28,24 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect-url')
     bag = request.session.get('bag', {})
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST.get('product_size')
+    if size:
+        if item_id in list(bag.keys()):
+            print("nor==", bag[item_id]['item_by_size'])
+            print("list ==", list(bag[item_id]['item_by_size']))
+            if size in bag[item_id]['item_by_size']:
+                bag[item_id]['item_by_size'][size] += quantity
+            else:
+                bag[item_id]['item_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'item_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
     request.session['bag'] = bag
     print('bag = ', request.session['bag'])
     return redirect(redirect_url)
