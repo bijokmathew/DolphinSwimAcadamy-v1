823bbd960fafde8cd2909c42c3d8cd34eb966f3c
bijokmathew <bijo.nbr@hotmail.com>
2023-03-19 19:06:18 +0000

    checkout-app: defined checkout view for checkout app

--- bag/views.py
+++ bag/views.py
@@ -7,7 +7,13 @@
 # -----------------------------------------------------------------
 # 3rd Party
 
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    HttpResponse,
+    get_object_or_404
+)
 from django.contrib import messages
 
 # internal
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+"""
+ Module view for checkout app
+"""
 
-# Create your views here.
+# Imports
+
+# -----------------------------------------------------------------
+# 3rd Party
+
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    get_object_or_404
+)
+from django.contrib import messages
+
+# internal
+from .models import Order, OrderLineItem
+# ------------------------------------------------------------------
+
+
+def checkout(request):
+    """
+    This function processes the checkout: the cart contents, user info and
+    the payment, validating it in the process.
+    Credit: Boutique Ado project, Code Institute.
+    Args:
+        request (object)
+    Returns:
+        The request object, the checkout template and the context.
+    """
+    bag = request.session.get('bag', {})
+    # prevet user to go to checkout by expliciltly typing checkout url
+    if not bag:
+        messages.error(request, "There is nothing in your bag at the momemt!")
+        return redirect(reverse('products'))
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, template, context=context)
