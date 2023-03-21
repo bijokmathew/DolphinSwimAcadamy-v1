eead440262fec52234088f5c687717967a0945b0
bijokmathew <bijo.nbr@hotmail.com>
2023-03-21 15:38:00 +0000

    Stripe : Added initial stripe payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,9 +14,12 @@ from django.shortcuts import (
     get_object_or_404
 )
 from django.contrib import messages
+from django.conf import settings
 
 # internal
+from bag.contexts import bag_contents
 from .forms import OrderForm
+import stripe
 # ------------------------------------------------------------------
 
 
@@ -30,16 +33,29 @@ def checkout(request):
     Returns:
         The request object, the checkout template and the context.
     """
+    # Get the stripe secret and public key from settings
+    stripePublicKey = settings.STRIPE_PUBLIC_KEY
+    clientSecret = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
+    current_bag = bag_contents(request)
     # prevet user to go to checkout by expliciltly typing checkout url
     if not bag:
         messages.error(request, "There is nothing in your bag at the momemt!")
         return redirect(reverse('products'))
+    stripe.api_key = clientSecret
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
     order_form = OrderForm()
     template = 'checkout/checkout.html'
+
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MnY04HYuD6x6xGkiGSdJWciyHvmodgWnGTqGewm2W4Y7dhvNGKqG1GrlI6VCaifVIaX28eWxpjndvgxxT1EDNKK00kR8Va1ln',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripePublicKey,
+        'client_secret': intent.client_secret
     }
     return render(request, template, context=context)
