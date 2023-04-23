aa3a99608d004ee9ceff75e0a2b2f16beda5cfda
bijokmathew <bijo.nbr@hotmail.com>
2023-04-23 07:58:15 +0000

    Inventory-Admin: modified button-style

--- products/forms.py
+++ products/forms.py
@@ -43,7 +43,7 @@ class ProductForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
         for filed_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'border-ocean-blue rounded-0'
 
     category = CustomMMCF(
         queryset=Category.objects.all(),
