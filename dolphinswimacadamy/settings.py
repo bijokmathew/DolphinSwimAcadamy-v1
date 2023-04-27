4fc6d2ec840155abc190b0791f0fa6938eb480d4
bijokmathew <bijo.nbr@hotmail.com>
2023-04-27 23:22:25 +0000

    Deployment: Added the code for deployment

--- dolphinswimacadamy/settings.py
+++ dolphinswimacadamy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-62e3lh4zx0cby*-f9c4tlrm_8%um8ih=48knzd_i8z-l_0l##i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dolphinswimacademy.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +126,17 @@ WSGI_APPLICATION = 'dolphinswimacadamy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
