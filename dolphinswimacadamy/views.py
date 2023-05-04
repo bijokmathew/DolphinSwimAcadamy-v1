e54df1ab9ada77374944689249bcb1e33a74469e
bijokmathew <bijo.nbr@hotmail.com>
2023-05-04 15:02:43 +0000

    Custom-Error: issue fixed template folder name are not matching

--- dolphinswimacadamy/views.py
+++ dolphinswimacadamy/views.py
@@ -14,4 +14,5 @@ from django.shortcuts import render
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+    print("======handler404=========")
+    return render(request, "error/404.html", status=404)
